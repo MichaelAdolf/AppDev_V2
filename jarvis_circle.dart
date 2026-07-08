9382ee8b6461dea854e54fdb966ea664d635a551
MichaelAdolf <mihi.adolf@gmail.com>
2026-07-08 19:57:13 +0200

    Update jarvis_circle.dart

--- jarvis_circle.dart
+++ jarvis_circle.dart
@@ -1,12 +1,76 @@
-8.7.2026, 19:51:08node: debug 12sensor.oppo_smartphone_next_alarm : msg.payload : string[11]
-"unavailable"
-8.7.2026, 19:51:08node: Return to Flutter Appmsg : string[18]
-"No response object"
-8.7.2026, 19:51:08node: Router Input Debugweather : msg.payload : Object
-{ success: true, intent: "dailyInfo", entity: "jarvis.daily_info", state: "executed", message: "Guten Abend. Heute ist Mittwoc…" }
-8.7.2026, 19:51:31node: debug 12sensor.oppo_smartphone_next_alarm : msg.payload : string[25]
-"2026-07-09T04:05:00+00:00"
-8.7.2026, 19:51:31node: Return to Flutter Appmsg : string[18]
-"No response object"
-8.7.2026, 19:51:31node: Router Input Debugweather : msg.payload : Object
-{ success: true, intent: "dailyInfo", entity: "jarvis.daily_info", state: "executed", message: "Guten Abend. Heute ist Mittwoc…" }
+PS D:\Users\Michael\Dokumente\16_AppDev\jarvis_app> flutter run
+Launching lib\main.dart on 25028RN03Y in debug mode...
+I/flutter (16321): [Jarvis WebSocket] WebSocket verbunden
+I/flutter (16321): [Jarvis WebSocket] WebSocket Fehler: WebSocketChannelException: Invalid argument (url): only ws: and wss: schemes are supported: Instance of '_SimpleUri'
+E/flutter (16321): [ERROR:flutter/runtime/dart_vm_initializer.cc(40)] Unhandled Exception: WebSocketChannelException: Invalid argument (url): only ws: and wss: schemes are supported: Instance of '_SimpleUri'
+E/flutter (16321): 
+WARNING: Your app uses the following plugins that apply Kotlin Gradle Plugin (KGP): flutter_tts, speech_to_text
+Future versions of Flutter will fail to build if your app uses plugins that apply KGP.
+
+Please check the changelogs of these plugins and upgrade to a version that supports Built-in Kotlin.
+If no such version exists, report the issue to the plugin. If necessary, here is a guide on filing 
+an issue against a plugin: https://docs.flutter.dev/release/breaking-changes/migrate-to-built-in-kotlin/for-app-developers#report-incompatible-kotlin-gradle-plugin-usage-to-plugin-authors
+
+If you are a plugin author, please migrate your plugin to Built-in Kotlin using this guide: https://docs.flutter.dev/release/breaking-changes/migrate-to-built-in-kotlin/for-plugin-authors
+I/flutter (16321): [Jarvis WebSocket] WebSocket verbunden
+I/flutter (16321): [Jarvis WebSocket] WebSocket Fehler: WebSocketChannelException: Invalid argument (url): only ws: and wss: schemes are supported: Instance of '_SimpleUri'
+E/flutter (16321): [ERROR:flutter/runtime/dart_vm_initializer.cc(40)] Unhandled Exception: WebSocketChannelException: Invalid argument (url): only ws: and wss: schemes are supported: Instance of '_SimpleUri'
+E/flutter (16321): 
+Running Gradle task 'assembleDebug'...                              5,4s
+√ Built build\app\outputs\flutter-apk\app-debug.apk
+Installing build\app\outputs\flutter-apk\app-debug.apk...           6,5s
+I/FlutterActivityAndFragmentDelegate(17505): If you are attempting to set --enable-dart-profiling via Intent extras to launch a Flutter component outside of using the Flutter CLI, note that support for setting engine flags on Android via Intent will soon be dropped; see https://github.com/flutter/flutter/issues/180686 for more information on this breaking change. To migrate, set --enable-dart-profiling or any other flags specified via Intent extras on the command line instead or see https://github.com/flutter/flutter/blob/main/docs/engine/Flutter-Android-Engine-Flags.md for alternative methods.
+D/FlutterJNI(17505): Beginning load of flutter...
+D/FlutterJNI(17505): flutter (null) was loaded normally!
+I/flutter (17505): [IMPORTANT:flutter/shell/platform/android/android_context_vk_impeller.cc(62)] Using the Impeller rendering backend (Vulkan).
+D/FlutterRenderer(17505): Width is zero. 0,0
+D/FlutterRenderer(17505): Width is zero. 0,0
+D/FlutterJNI(17505): Sending viewport metrics to the engine.
+Syncing files to device 25028RN03Y...                               67ms
+
+Flutter run key commands.
+r Hot reload. 
+R Hot restart.
+h List all available interactive commands.
+d Detach (terminate "flutter run" but leave application running).
+c Clear the screen
+q Quit (terminate the application on the device).
+
+A Dart VM Service on 25028RN03Y is available at: http://127.0.0.1:60547/IOozQYCnPTQ=/
+The Flutter DevTools debugger and profiler on 25028RN03Y is available at:
+http://127.0.0.1:60547/IOozQYCnPTQ=/devtools/?uri=ws://127.0.0.1:60547/IOozQYCnPTQ=/ws
+I/flutter (17505): [Jarvis WebSocket] WebSocket verbunden
+I/flutter (17505): [Jarvis WebSocket] WebSocket Fehler: WebSocketChannelException: Invalid argument (url): only ws: and wss: schemes are supported: Instance of '_SimpleUri'
+E/flutter (17505): [ERROR:flutter/runtime/dart_vm_initializer.cc(40)] Unhandled Exception: WebSocketChannelException: Invalid argument (url): only ws: and wss: schemes are supported: Instance of '_SimpleUri'
+E/flutter (17505): 
+W/Choreographer(17505): Already have a pending vsync event.  There should only be one at a time.
+I/Choreographer(17505): Skipped 356 frames!  The application may be doing too much work on its main thread.
+I/mple.jarvis_app(17505): Background concurrent mark compact GC freed 2184KB AllocSpace bytes, 0(0B) LOS objects, 49% free, 2776KB/5553KB, paused 683us,7.883ms total 51.707ms
+I/mple.jarvis_app(17505): Background concurrent mark compact GC freed 2752KB AllocSpace bytes, 0(0B) LOS objects, 49% free, 2808KB/5617KB, paused 815us,7.834ms total 50.725ms
+I/Choreographer(17505): Skipped 43 frames!  The application may be doing too much work on its main thread.
+D/mali_gralloc(17505): register: id=235000000ad, handle=0xb400007d39c503a0, importpid=17505
+I/HWUI    (17505): Davey! duration=754ms; Flags=1, FrameTimelineVsyncId=123248, IntendedVsync=2364410157462, Vsync=2365126781702, InputEventId=0, HandleInputStart=2365133059653, AnimationStart=2365133065615, PerformTraversalsStart=2365133068499, DrawStart=2365138412846, FrameDeadline=2364426157462, FrameInterval=2365132188115, FrameStartTime=16665680, SyncQueued=2365146553615, SyncStart=2365146688192, IssueDrawCommandsStart=2365147738423, SwapBuffers=2365161761692, FrameCompleted=2365164697423, DequeueBufferDuration=2818076, QueueBufferDuration=1246923, GpuCompleted=2365162384384, SwapBuffersCompleted=2365164697423, DisplayPresentTime=0, CommandSubmissionCompleted=2365161761692, 
+D/mali_gralloc(17505): unregister: id=235000000aa, handle=0xb400007d1c3fb560, base=0x0, importpid=17505, clone_count=0
+D/mali_gralloc(17505): unregister: id=235000000ab, handle=0xb400007d1c3fb640, base=0x0, importpid=17505, clone_count=0
+D/mali_gralloc(17505): unregister: id=235000000ac, handle=0xb400007d1c3fb720, base=0x0, importpid=17505, clone_count=0
+D/mali_gralloc(17505): unregister: id=235000000a9, handle=0xb400007d1c3fb480, base=0x0, importpid=17505, clone_count=0
+D/mali_gralloc(17505): unregister: id=235000000a8, handle=0xb400007d13de5c80, base=0x0, importpid=17505, clone_count=0
+D/ProfileInstaller(17505): Installing profile for com.example.jarvis_app
+D/mali_gralloc(17505): unregister: id=235000000ad, handle=0xb400007d39c503a0, base=0x0, importpid=17505, clone_count=0
+D/FlutterJNI(17505): Sending viewport metrics to the engine.
+I/flutter (17505): [Jarvis WebSocket] WebSocket verbunden
+I/flutter (17505): [Jarvis WebSocket] WebSocket Fehler: WebSocketChannelException: Invalid argument (url): only ws: and wss: schemes are supported: Instance of '_SimpleUri'
+E/flutter (17505): [ERROR:flutter/runtime/dart_vm_initializer.cc(40)] Unhandled Exception: WebSocketChannelException: Invalid argument (url): only ws: and wss: schemes are supported: Instance of '_SimpleUri'
+E/flutter (17505): 
+I/flutter (17505): [Jarvis WebSocket] WebSocket verbunden
+I/flutter (17505): [Jarvis WebSocket] WebSocket Fehler: WebSocketChannelException: Invalid argument (url): only ws: and wss: schemes are supported: Instance of '_SimpleUri'
+E/flutter (17505): [ERROR:flutter/runtime/dart_vm_initializer.cc(40)] Unhandled Exception: WebSocketChannelException: Invalid argument (url): only ws: and wss: schemes are supported: Instance of '_SimpleUri'
+E/flutter (17505): 
+I/flutter (17505): [Jarvis WebSocket] WebSocket verbunden
+I/flutter (17505): [Jarvis WebSocket] WebSocket Fehler: WebSocketChannelException: Invalid argument (url): only ws: and wss: schemes are supported: Instance of '_SimpleUri'
+E/flutter (17505): [ERROR:flutter/runtime/dart_vm_initializer.cc(40)] Unhandled Exception: WebSocketChannelException: Invalid argument (url): only ws: and wss: schemes are supported: Instance of '_SimpleUri'
+E/flutter (17505): 
+I/flutter (17505): [Jarvis WebSocket] WebSocket verbunden
+I/flutter (17505): [Jarvis WebSocket] WebSocket Fehler: WebSocketChannelException: Invalid argument (url): only ws: and wss: schemes are supported: Instance of '_SimpleUri'
+E/flutter (17505): [ERROR:flutter/runtime/dart_vm_initializer.cc(40)] Unhandled Exception: WebSocketChannelException: Invalid argument (url): only ws: and wss: schemes are supported: Instance of '_SimpleUri'
+E/flutter (17505): 
