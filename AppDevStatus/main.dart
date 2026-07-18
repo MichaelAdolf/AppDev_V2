1e7493dba71012747a679882a9231053d7a480e5
MichaelAdolf <mihi.adolf@gmail.com>
2026-07-18 11:59:22 +0200

    Add files via upload

---
+++ AppDevStatus/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+import 'features/jarvis/ui/home_screen.dart';
+import 'features/jarvis/logic/jarvis_controller.dart';
+
+import 'services/home_assistant_service.dart';
+import 'services/real_home_assistant_service.dart';
+import 'services/jarvis_background_bridge.dart';
+import 'services/device_wake_service.dart';
+import 'services/jarvis_wakeword_service.dart';
+//import 'services/mock_home_assistant_service.dart';
+import 'core/constants.dart';
+import 'core/theme.dart';
+import 'core/ha_response.dart';
+import 'dart:convert';
+
+
+Future<void> main() async {
+  // final HomeAssistantService haService =
+  //    MockHomeAssistantService();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final HomeAssistantService haService =
+      RealHomeAssistantService(
+        baseUrl: NodeRedConfig.baseUrl,
+        token: '',
+      );
+
+  // Später:
+  //
+  // final HomeAssistantService haService =
+  //     RealHomeAssistantService(
+  //       baseUrl: 'http://192.168.178.50:8123',
+  //       token: 'TOKEN',
+  //     );
+
+  final controller = JarvisController(
+    homeAssistantService: haService,
+  );
+
+  JarvisBackgroundBridge.initialize(
+    onEvent: (event) async { 
+      print( '[JARVIS BACKGROUND RAW] $event', );
+    
+    try {
+      final decoded =
+          jsonDecode(event) as Map<String, dynamic>;
+
+      final response =
+          HaResponse.fromJson(decoded);
+
+      await DeviceWakeService.wakeDevice();
+
+      await controller.handleExternalResponse(
+        response,
+        source: 'android-service',
+      );
+    } catch (e) {
+      print(
+        '[JARVIS BACKGROUND ERROR] $e',
+      );
+      }
+    }, 
+  );
+
+  runApp(
+    JarvisApp(
+      controller: controller,
+    ),
+  );
+
+  final wakewordService = JarvisWakewordService();
+
+  await wakewordService.initialize(
+    onWakeword: (){
+      print(
+        '[JARVIS] Wakeword erkannt',
+      );
+    }
+  );
+}
+
+class JarvisApp extends StatelessWidget {
+  final JarvisController controller;
+
+  const JarvisApp({
+    super.key,
+    required this.controller,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: JarvisTheme.darkTheme,
+      home: HomeScreen(
+        controller: controller,
+      ),
+    );
+  }
+}
