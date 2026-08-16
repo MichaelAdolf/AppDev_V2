c08e31f0ab1b06bc10c0a08c2716ec7f3359e4fb
MichaelAdolf <mihi.adolf@gmail.com>
2026-08-16 16:16:49 +0200

    Delete AppDevStatus/jarvis_wakeword_bus.dart

--- AppDevStatus/jarvis_wakeword_bus.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:async';
-
-class JarvisWakewordBus {
-
-static final StreamController _controller = StreamController.broadcast();
-
-static Stream get stream => _controller.stream;
-
-static void fire() { _controller.add(null); } }
