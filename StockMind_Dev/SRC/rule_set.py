726b3ad96139ea74433a0a73df5f8a541ea7b27f
MichaelAdolf <mihi.adolf@gmail.com>
2026-09-23 21:51:18 +0200

    Delete StockMind_Dev/SRC directory

--- StockMind_Dev/SRC/rule_set.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-from stockmind.domain.rules.base_rule import ( BaseRule )
-
-@dataclass(frozen=True)
-class RuleSet:
-    name: str
-    
-    rules: list[BaseRule]
